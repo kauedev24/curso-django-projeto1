2514e3a02afa2484fdf01db9dffb293a9e6f82b9
Kaue Brito <knascimento353@gmail.com>
2023-10-22 12:19:18 -0300

    new example changing the domain

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from recipes.views import home, contact, about
 
 
+# dominio/recipes -> home
 urlpatterns = [
     path('', home),  # Home
     path('about/', about),  # /about/
