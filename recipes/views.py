11637d955249f847dacbd7036d58cdc1e1fb1025
Kaue Brito <knascimento353@gmail.com>
2023-10-22 12:58:59 -0300

    Templates and HTML rendering in Django

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,18 @@
-"""My Views"""
+"""
+My Views
+render ->> read a file and render, return HttpResponse
+"""
 
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    """return HTTP response"""
-    return HttpResponse("Home 2")
+    """return HTTP response ->> render"""
+    # return HttpResponse("Home 2")
+    return render(request, 'recipes/home.html')
 
 
 def contact(request):
